842a22a1e6790a156e5c3d5da0cedc669f193cea
dylanR96 <dylan.ravenson@gmail.com>
2024-02-12 15:48:03 +0100

    Added error handling for IP address

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 let enteredIp = document.getElementById("input-ip");
 const myButton = document.getElementById("button");
 const containerDiv = document.getElementById("ip-info-container");
+const errorContainer = document.getElementById("errorMessage");
 myButton.addEventListener("click", (event) => {
   event.preventDefault();
-  myIp();
+  containerDiv.textContent = "";
+  testIp(enteredIp.value);
 });
-
-function myIp() {
-
-  enteredIp = enteredIp.value;
-  myApi(enteredIp);
-}
-
 
 async function myApi(enteredIp) {
   const geoUrl = `https://geo.ipify.org/api/v2/country,city?apiKey=at_oFYCxTsdHGChCtTazoopzGKYrhp7M&ipAddress=${enteredIp}`;
@@ -22,7 +17,7 @@ async function myApi(enteredIp) {
     myMap(lat, lng);
     let ipData = enteredIp;
     let locationData = `${data.location.city}, ${data.location.region} ${data.location.country}`;
-    let timeZData = data.location.timezone;
+    let timeZData = `UTC ${data.location.timezone}`;
     let ispData = data.isp;
     addInfo(ipData, locationData, timeZData, ispData);
   } catch (error) {
@@ -39,17 +34,27 @@ function addInfo(ipData, locationData, timeZoneData, ispData) {
 }
 
 function createAndAppend() {
-  let newDiv = document.createElement("div");
+  newDiv = document.createElement("div");
   containerDiv.appendChild(newDiv);
   return newDiv;
 }
 
+let map = L.map('map').setView([0, 0], 19);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
 function myMap(lat, lng) {
-  let map = L.map('map').setView([lat, lng], 19);
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-  }).addTo(map);
+  map.panTo([lat, lng]);
 }
 
+function testIp(ipAddress) {
+  if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipAddress)) {
+    myApi(enteredIp.value);
+  } else {
+    errorContainer.textContent = "Invalid IP-address!";
+  }
+}
+//91.145.50.107
+//139.47.39.174
